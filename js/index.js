f1010cf622969407ab5946d47495bf603e515ade
solomom399 <sobanjosodiq@gmail.com>
2017-03-09 15:23:07 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,9 +12,9 @@ function onDeviceReady() {
 
 
 function dbcopy() {
-        var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
-        
-        myDB.transaction(function(transaction) {
+        //var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
+        var myDB = window.sqlitePlugin.openDatabase({ name : "mydb.db", createFromLocation : 1 });
+        /*myDB.transaction(function(transaction) {
             transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
             function(tx, result) {
                 alert("Table created successfully");
@@ -35,15 +35,15 @@ function dbcopy() {
             function(error){
                 alert('Error occurred');
             });
-        });
+        });*/
     
     
         myDB.transaction(function(transaction) {
-            transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
+            transaction.executeSql('SELECT * FROM user', [], function (tx, results) {
                 var len = results.rows.length, i;
                 $("#rowCount").append(len);
                 for (i = 0; i < len; i++){
-                    $("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td></tr>");
+                    $("#TableData").append("<tr><td>"+results.rows.item(i).name+"</td></tr>");
                 }
             }, null);
         });
