16095c21ef404d49e61d174bcefcfa677b233109
solomom399 <sobanjosodiq@gmail.com>
2017-03-09 05:14:14 -0800

    First commit

--- js/index.js
+++ js/index.js
@@ -1,52 +1,50 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onDeviceReady() {
+    // var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+
+    dbcopy()
+}
+
+
+
+function dbcopy() {
+        //Database filename to be copied is demo.db
+
+        //location = 0, will copy the db to default SQLite Database Directory
+        window.plugins.sqlDB.copy("mydb.db", 0, copysuccess,copyerror);
+
+        // or
+
+        //location = 1, will copy the database to /Library folder
+        // window.plugins.sqlDB.copy("demo.db", 1, copysuccess,copyerror);
+
+        // or
+
+        //location = 2, will copy the database to /Library/LocalDatabase folder (Disable iCloud Backup)
+        // window.plugins.sqlDB.copy("demo.db", 2, copysuccess,copyerror);
+
+}
+
+function removeDB() {
+      var location = 1;
+      window.plugins.sqlDB.remove("mydb.db", location, rmsuccess,rmerror);  
+}
+
+function copysuccess() {
+        //open db and run your queries
+         db = window.sqlitePlugin.openDatabase({name: "mydb.db"});.
+}
+
+function copyerror(e) {
+        //db already exists or problem in copying the db file. Check the Log.
+        $("#df").html("Error Code = "+JSON.stringify(e));
+        //e.code = 516 => if db exists
+}
+
+
+
 
 $(function (){
   function cameraSuccess(imagedata){
